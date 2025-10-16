b937803532cba7f6d2fc26af006e4d57f81f36a6
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-10-16 18:17:25 +0900

    fix: remove codes for test (#3)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import PokemonList from "@/components/PokemonList";
-
 export default async function Home() {
-  return (
-    <div className="flex flex-col p-10 gap-2">
-      <PokemonList />
-    </div>
-  );
+  return <div></div>;
 }
