d7ff1dc630c2cede12071fd36da72ade78ac4da2
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-10-19 13:45:47 +0900

    fix: wrap whole card with Link component (#5)

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -8,27 +8,30 @@ interface PokemonCardProps {
 
 export default function PokemonCard({ pokemon }: PokemonCardProps) {
   return (
-    <div className="flex flex-col relative gap-1 overflow-hidden justify-center items-center px-8 py-10 border-b-4 border-r-4 border-gray-800 rounded-xl bg-neutral-50 cursor-pointer hover:scale-105 transition-transform">
+    <Link
+      href={`/${pokemon.id}`}
+      className="flex flex-col relative gap-1 overflow-hidden justify-center items-center px-8 py-10 border-b-4 border-r-4 border-gray-800 rounded-xl bg-neutral-50 cursor-pointer hover:scale-105 transition-transform"
+    >
       <div className="w-full absolute top-0">
         <div className="h-2 bg-red-500" />
         <div className="h-2 bg-black" />
       </div>
+
       <Image
         src={pokemon.frontImageUrl}
         alt={`${pokemon.name}-image`}
         width={90}
         height={90}
       />
-      <Link href={`/${pokemon.id}`}>
-        <span className="text-lg font-medium hover:underline hover:font-semibold">
-          {pokemon.name}
-        </span>
-      </Link>
+
+      <span className="text-lg font-medium hover:underline hover:font-semibold">
+        {pokemon.name}
+      </span>
 
       <div className="w-full absolute bottom-0">
         <div className="h-2 bg-black" />
         <div className="h-2 bg-red-500" />
       </div>
-    </div>
+    </Link>
   );
 }
