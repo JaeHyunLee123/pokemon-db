9c5b74c2d94c1b41d1bf86011c8ae5b528cce183
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-10-26 10:51:49 +0900

    feat: fetch pokemon detail data (#21)

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,9 +1,16 @@
+import { pokemonService } from "@/services/pokemon-services";
+
 export default async function Page({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
+  const pokemonDetail = await pokemonService.getById(Number(id));
 
-  return <div>{`Detail of ${id}`}</div>;
+  if (!pokemonDetail) {
+    return <div>서버에 에러가 발생했습니다. 잠시 후 다시 시도해주세요.</div>;
+  }
+
+  return <div>{`Detail of ${pokemonDetail.name}`}</div>;
 }
