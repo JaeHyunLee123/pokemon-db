526560aab26e0e679f9c706d32572a9ff486fc39
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-10-26 11:22:22 +0900

    feat: render basic information of pokemon (#21)

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -13,16 +13,25 @@ export default async function Page({
     return <div>서버에 에러가 발생했습니다. 잠시 후 다시 시도해주세요.</div>;
   }
 
-  const { name, frontImageUrl, backImageUrl } = pokemonDetail;
+  const { name, frontImageUrl, backImageUrl, description, types } =
+    pokemonDetail;
 
   return (
-    <div className="flex flex-col items-center p-10">
+    <div className="flex flex-col items-center p-10 gap-2">
       <PokemonImageCard
         frontImageUrl={frontImageUrl}
         backImageUrl={backImageUrl}
         pokemonName={name}
       />
-      {`Detail of ${name}`}
+      <h1 className="text-2xl">{name}</h1>
+
+      <p>{description}</p>
+
+      <div className="flex items-center justify-center gap-1">
+        {types.map((type, i) => (
+          <span key={i}>{type}</span>
+        ))}
+      </div>
     </div>
   );
 }
