36165e566668a5c315f9b1f2be9d5dead1864f38
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-10-27 21:24:42 +0900

    feat: PokemonSearchInput (#24)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,23 @@
+"use client";
+
+import PokemonSearchInput from "@/components/PokemonSearchInput";
+import { cn } from "@/libs/utils";
+import { usePathname } from "next/navigation";
+
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <header className="sticky w-full">
       <div className="bg-red-500 h-14" />
       <div className="bg-black h-14 flex items-center justify-center">
         <h1 className="text-white font-semibold text-2xl">포켓몬 DB</h1>
       </div>
-      <div className="bg-white h-14" />
+      <div className="bg-white h-14 flex justify-center items-center px-1">
+        <PokemonSearchInput
+          className={cn(pathname === "/" ? "visible" : "hidden")}
+        />
+      </div>
     </header>
   );
 }
---
+++ src/components/PokemonSearchInput.tsx
@@ -0,0 +1,27 @@
+import useSearchStore from "@/hooks/stores/useSearchStore";
+import { cn } from "@/libs/utils";
+
+interface PokemonSearchInputProps {
+  className?: string;
+}
+
+export default function PokemonSearchInput({
+  className,
+}: PokemonSearchInputProps) {
+  const { setSearch, search } = useSearchStore();
+
+  return (
+    <input
+      className={cn(
+        "bg-neutral-50 rounded p-2 max-w-md w-full border focus:outline-none ",
+        className
+      )}
+      placeholder="포켓몬 이름으로 검색해보세요!"
+      value={search}
+      onChange={(e) => {
+        e.preventDefault();
+        setSearch(e.currentTarget.value);
+      }}
+    />
+  );
+}
