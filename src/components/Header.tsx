57866c065e5d03c436868e50029239862f1d5eae
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-10-27 21:25:31 +0900

    fix: add home link to title (#24)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@
 
 import PokemonSearchInput from "@/components/PokemonSearchInput";
 import { cn } from "@/libs/utils";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function Header() {
@@ -11,7 +12,9 @@ export default function Header() {
     <header className="sticky w-full">
       <div className="bg-red-500 h-14" />
       <div className="bg-black h-14 flex items-center justify-center">
-        <h1 className="text-white font-semibold text-2xl">포켓몬 DB</h1>
+        <Link href={"/"}>
+          <h1 className="text-white font-semibold text-2xl">포켓몬 DB</h1>
+        </Link>
       </div>
       <div className="bg-white h-14 flex justify-center items-center px-1">
         <PokemonSearchInput
