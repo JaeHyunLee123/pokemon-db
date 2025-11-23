435580b434c293376249f86cd7a630933b67044e
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-11-23 18:21:57 +0900

    feat: 비밀번호 보이기 토글 기능추가 (#32)

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/libs/utils";
-import { ComponentProps } from "react";
+import { ComponentProps, useState } from "react";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
 
 interface InputProps extends ComponentProps<"input"> {
   inputClassName?: string;
@@ -12,18 +13,36 @@ export default function Input({
   label,
   inputClassName,
   errorMessage,
+  type,
   ...props
 }: InputProps) {
+  const [isPasswordHide, setIsPasswordHide] = useState(type === "password");
+
   return (
     <label className={cn("flex flex-col gap-1", className)}>
       <span className="text-sm">{label}</span>
-      <input
-        className={cn(
-          "bg-neutral-50 p-2 focus:outline-none border-t border-l border-b-2 border-r-2 border-black",
-          inputClassName
-        )}
-        {...props}
-      />
+      <div className="flex items-center gap-1">
+        <input
+          className={cn(
+            "bg-neutral-50 p-2 focus:outline-none border-t border-l border-b-2 border-r-2 border-black flex-1",
+            inputClassName
+          )}
+          type={
+            isPasswordHide ? "password" : type === "password" ? "text" : type
+          }
+          {...props}
+        />
+        {type === "password" ? (
+          <button
+            onClick={() => {
+              setIsPasswordHide((prev) => !prev);
+            }}
+            type={"button"}
+          >
+            {isPasswordHide ? <EyeIcon /> : <EyeOffIcon />}
+          </button>
+        ) : null}
+      </div>
       <span className="text-xs text-red-500">{errorMessage}</span>
     </label>
   );
