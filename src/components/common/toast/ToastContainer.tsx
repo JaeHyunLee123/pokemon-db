1822d50606ab3b928f91e8be764aaf907b297f61
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-11-25 19:17:58 +0900

    fix: 불필요한 useEffect 제거(#37)

--- src/components/common/toast/ToastContainer.tsx
+++ src/components/common/toast/ToastContainer.tsx
@@ -1,21 +1,15 @@
 "use client";
 
-import type { Toast } from "@/types/types";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import ToastItem from "@/components/common/toast/ToastItem";
 import ToastContext from "@/libs/contexts/ToastContext";
 
 export default function ToastContainer() {
-  const [renderedToasts, setRenderedToasts] = useState<Toast[]>([]);
   const { toasts } = useContext(ToastContext);
-
-  useEffect(() => {
-    setRenderedToasts(toasts);
-  }, [toasts]);
 
   return (
     <div className="fixed top-18 right-1 z-500 w-full max-w-md space-y-1 p-1">
-      {renderedToasts.map(({ id, type, title, content }) => (
+      {toasts.map(({ id, type, title, content }) => (
         <ToastItem
           key={id}
           id={id}
