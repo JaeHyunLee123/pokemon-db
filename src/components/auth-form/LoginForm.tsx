02f3caa4c99a0e00ce792d1d59191ab017ace406
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-11-27 15:01:20 +0900

    feat: 로그인 폼 로직 (#33)

--- src/components/auth-form/LoginForm.tsx
+++ src/components/auth-form/LoginForm.tsx
@@ -3,23 +3,51 @@
 import Button from "@/components/common/Button";
 import Input from "@/components/common/Input";
 import { cn } from "@/libs/utils";
+import { LoginFormSchema, LoginFormType } from "@/schemas/login-schema";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { ComponentProps } from "react";
+import { useForm } from "react-hook-form";
 
 export default function LoginForm({
   className,
   ...props
 }: ComponentProps<"form">) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormType>({
+    resolver: zodResolver(LoginFormSchema),
+  });
+
+  const onSubmit = (form: LoginFormType) => {
+    //TODO: api 연결
+    console.log(form);
+  };
+
   return (
-    <form className={cn("flex flex-col gap-2", className)} {...props}>
+    <form
+      className={cn("flex flex-col gap-2", className)}
+      onSubmit={handleSubmit(onSubmit)}
+      {...props}
+    >
       <Input
         label="이메일"
         placeholder="example@example.com"
         type="email"
         aria-label="email-input"
+        {...register("email")}
+        errorMessage={errors.email?.message}
       />
-      <Input label="비밀번호" type="password" aria-label="password-input" />
+      <Input
+        label="비밀번호"
+        type="password"
+        aria-label="password-input"
+        {...register("password")}
+        errorMessage={errors.password?.message}
+      />
 
-      <Button type="submit" aria-label="sign-up-button">
+      <Button type="submit" aria-label="login-button">
         {"로그인"}
       </Button>
     </form>
