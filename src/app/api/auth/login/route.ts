8201d9bc8da8c30c1b88cb8662acd1b67ebe835f
jhyon123@gmail.com <ljh@JaeHyunLees-MacBook-Air.local>
2026-02-01 15:37:37 +0900

    fix: change login route handler from GET to POST (#33)

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,24 +1,26 @@
 import { IncorrectPasswordError, NoUserError } from "@/errors/auth-error";
+import { LoginFormSchema } from "@/schemas/login-schema";
 import { authService } from "@/services/auth-services";
 import { NextRequest, NextResponse } from "next/server";
+import z from "zod";
 
-export async function GET(req: NextRequest) {
-  const { searchParams } = new URL(req.url);
-
-  const email = searchParams.get("email");
-  const password = searchParams.get("password");
-
-  if (!(email && password)) {
-    return NextResponse.json(
-      { message: "Invalid request body" },
-      { status: 400 }
-    );
-  }
-
+export async function POST(req: NextRequest) {
   try {
+    const json = await req.json();
+
+    const { email, password } = LoginFormSchema.parse(json);
+
     await authService.login(email, password);
     return NextResponse.json({}, { status: 200 });
   } catch (e) {
+    // zod validation error
+    if (e instanceof z.ZodError) {
+      return NextResponse.json(
+        { message: "Invalid request body" },
+        { status: 400 },
+      );
+    }
+
     if (e instanceof NoUserError) {
       return NextResponse.json({ message: "No User" }, { status: 404 });
     }
@@ -26,13 +28,13 @@ export async function GET(req: NextRequest) {
     if (e instanceof IncorrectPasswordError) {
       return NextResponse.json(
         { message: "incorrect password" },
-        { status: 401 }
+        { status: 401 },
       );
     }
 
     return NextResponse.json(
       { message: "unknown server error" },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
